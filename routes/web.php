6088ef3dd7877c6bd8bcfb3ff7fc99373998a5c5
JavizzzG <javierstivengalvis2008@gmail.com>
2025-07-27 01:42:31 -0500

    cambio temporal

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,15 @@ use App\Http\Controllers\CursoController;
 use App\Http\Controllers\InscripcionController;
 
 
-Route::get('/', [UsuarioController::class, 'index'])->name('index');
+// Route::get('/', [UsuarioController::class, 'index'])->name('index');
+Route::get('/', function () {
+    return response()->json([
+        'status' => 'Laravel arrancó correctamente',
+        'app_env' => env('APP_ENV'),
+        'debug' => env('APP_DEBUG'),
+    ]);
+});
+
 Route::get('inicio', function () {
     return view('usuarios.inicio');
 })->name('inicio')->middleware('auth');
